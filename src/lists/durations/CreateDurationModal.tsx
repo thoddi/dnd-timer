c25f4a6aab9122822ef50f19fcf323b552b4be7e
Þorvarður Bergmann Kjartansson <thoddi@outlook.com>
2024-03-25 21:26:05 +0000

    Stilling tímabils
    Útfært notendavænni leið til að velja lengd tímabils.

--- src/lists/durations/CreateDurationModal.tsx
+++ src/lists/durations/CreateDurationModal.tsx
@@ -2,30 +2,86 @@ import { useContext, useState } from "react";
 import FormGroup from "../../utilityComponents/FormGroup";
 import Modal from "../../utilityComponents/Modal";
 import { DurationContext } from "../../contexts/AppContextProvider";
+import InputGroup from "react-bootstrap/esm/InputGroup";
+import DropdownButton from "react-bootstrap/esm/DropdownButton";
+import Dropdown from "react-bootstrap/esm/Dropdown";
+import Form from "react-bootstrap/esm/Form";
+
+interface TimeUnit {
+  name: string;
+  seconds: number;
+}
+interface UnitsOfTime {
+  [key: string]: TimeUnit;
+}
+
+const unitsOfTime: UnitsOfTime = {
+  seconds: {
+    name: 'sekúndur',
+    seconds: 1,
+  },
+  minutes: {
+    name: 'mínútur',
+    seconds: 60,
+  },
+  hours: {
+    name: 'klukkutímar',
+    seconds: 3_600,
+  },
+  days: {
+    name: 'dagar',
+    seconds: 86_400,
+  },
+  months: {
+    name: 'mánuðir',
+    seconds: 2_592_000, // 30 dagar
+  },
+  years: {
+    name: 'ár',
+    seconds: 31_104_000,
+  },
+};
 
 interface Props {
   show: boolean;
   onHide(): void;
 }
 
-function CreateDurationModal({ show, onHide }: Props) {
-  const defaultDuration = 3600; // 1 hour
+function CreateDurationModal({ show, onHide: _onHide }: Props) {
   const [name, setName] = useState('');
-  const [duration, setDuration] = useState(defaultDuration); // 1 hour
+  const [duration, setDuration] = useState(1);
+  const [unit, setUnit] = useState('hours');
 
   const { add } = useContext(DurationContext);
 
   const onSave = () => {
-    add({ name, duration });
-    setName('');
-    setDuration(defaultDuration);
+    add({ name, duration: duration * unitsOfTime[unit].seconds });
     onHide();
   };
+
+  const onHide = () => {
+    _onHide();
+    setName('');
+    setDuration(1);
+  };
+
 
   return (
     <Modal show={show} title="Vista nýtt tímabil" onHide={onHide} primaryBtnText="Vista" onPrimaryBtnClick={onSave}>
       <FormGroup label="Heiti" value={name} onChange={setName}></FormGroup>
-      <FormGroup type="number" label="Tímalengd" value={duration} onChange={(value?: number) => setDuration(value ?? 0)}></FormGroup>
+      <Form.Label>Tímalengd</Form.Label>
+      <InputGroup>
+        <Form.Control type="number" value={duration} onChange={({ currentTarget }) => setDuration(Number(currentTarget.value))} />
+        <DropdownButton
+          variant="outline-secondary"
+          align="end"
+          title={unitsOfTime[unit].name}>
+            {Object.entries(unitsOfTime).map(([key, value]) => (
+              <Dropdown.Item active={unit === key} onClick={() => setUnit(key)}>{value.name}</Dropdown.Item>
+            ))}
+        </DropdownButton>
+      </InputGroup>
+      
     </Modal>
   )
 }
--- src/timers/TimerList.tsx
+++ src/timers/TimerList.tsx
@@ -1,19 +1,12 @@
 import { useContext } from "react";
 import { TimerContext } from "../contexts/AppContextProvider";
 import TimerItem from "./Timer";
-import Card from "react-bootstrap/Card";
-import CardHeader from "react-bootstrap/esm/CardHeader";
 
 function TimerList() {
   const { list: timers } = useContext(TimerContext);
 
   return (
     <div>
-      {/* <Card>
-        <CardHeader className="d-flex justify-content-between">
-          <h5>Teljarar</h5>
-        </CardHeader>
-      </Card> */}
       <h2>Teljarar</h2>
       {timers.map((timer) => (
         <TimerItem key={timer.id}
