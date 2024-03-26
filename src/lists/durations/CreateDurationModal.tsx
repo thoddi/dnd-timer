b25c73e082ab813e62416ea8c2ab05de236b19db
Þorvarður Bergmann Kjartansson <thoddi@outlook.com>
2024-03-26 22:38:41 +0000

    Búið til InputDuration
    Notað það þar sem þarf að velja lengd tímabils.

--- src/lists/durations/CreateDurationModal.tsx
+++ src/lists/durations/CreateDurationModal.tsx
@@ -2,45 +2,10 @@ import { useContext, useState } from "react";
 import FormGroup from "../../utilityComponents/FormGroup";
 import Modal from "../../utilityComponents/Modal";
 import { DurationContext } from "../../contexts/AppContextProvider";
-import InputGroup from "react-bootstrap/esm/InputGroup";
-import DropdownButton from "react-bootstrap/esm/DropdownButton";
-import Dropdown from "react-bootstrap/esm/Dropdown";
 import Form from "react-bootstrap/esm/Form";
+import InputDuration from "../../utilityComponents/InputDuration";
 
-interface TimeUnit {
-  name: string;
-  seconds: number;
-}
-interface UnitsOfTime {
-  [key: string]: TimeUnit;
-}
-
-const unitsOfTime: UnitsOfTime = {
-  seconds: {
-    name: 'sekúndur',
-    seconds: 1,
-  },
-  minutes: {
-    name: 'mínútur',
-    seconds: 60,
-  },
-  hours: {
-    name: 'klukkutímar',
-    seconds: 3_600,
-  },
-  days: {
-    name: 'dagar',
-    seconds: 86_400,
-  },
-  months: {
-    name: 'mánuðir',
-    seconds: 2_592_000, // 30 dagar
-  },
-  years: {
-    name: 'ár',
-    seconds: 31_104_000,
-  },
-};
+const initSeconds = 3600; // 1 klukkutími
 
 interface Props {
   show: boolean;
@@ -49,20 +14,19 @@ interface Props {
 
 function CreateDurationModal({ show, onHide: _onHide }: Props) {
   const [name, setName] = useState('');
-  const [duration, setDuration] = useState(1);
-  const [unit, setUnit] = useState('hours');
+  const [duration, setDuration] = useState(initSeconds);
 
   const { add } = useContext(DurationContext);
 
   const onSave = () => {
-    add({ name, duration: duration * unitsOfTime[unit].seconds });
+    add({ name, duration: duration });
     onHide();
   };
 
   const onHide = () => {
     _onHide();
     setName('');
-    setDuration(1);
+    setDuration(initSeconds);
   };
 
 
@@ -70,18 +34,7 @@ function CreateDurationModal({ show, onHide: _onHide }: Props) {
     <Modal show={show} title="Vista nýtt tímabil" onHide={onHide} primaryBtnText="Vista" onPrimaryBtnClick={onSave}>
       <FormGroup label="Heiti" value={name} onChange={setName}></FormGroup>
       <Form.Label>Tímalengd</Form.Label>
-      <InputGroup>
-        <Form.Control type="number" value={duration} onChange={({ currentTarget }) => setDuration(Number(currentTarget.value))} />
-        <DropdownButton
-          variant="outline-secondary"
-          align="end"
-          title={unitsOfTime[unit].name}>
-            {Object.entries(unitsOfTime).map(([key, value]) => (
-              <Dropdown.Item active={unit === key} onClick={() => setUnit(key)}>{value.name}</Dropdown.Item>
-            ))}
-        </DropdownButton>
-      </InputGroup>
-      
+      <InputDuration value={duration} onChange={setDuration}></InputDuration>
     </Modal>
   )
 }
--- src/timers/CreateTimerModal.tsx
+++ src/timers/CreateTimerModal.tsx
@@ -2,6 +2,8 @@ import { useContext, useState } from "react";
 import { TimerContext } from "../contexts/AppContextProvider";
 import Modal from "../utilityComponents/Modal";
 import FormGroup from "../utilityComponents/FormGroup";
+import { Form } from "react-bootstrap";
+import InputDuration from "../utilityComponents/InputDuration";
 
 interface Props {
   show: boolean;
@@ -11,20 +13,27 @@ interface Props {
   logId?: number;
 }
 
-function CreateTimerModal({ show, onHide, durationId, eventId, logId }: Props) {
+function CreateTimerModal({ show, onHide: _onHide, durationId, eventId, logId }: Props) {
   const [name, setName] = useState('');
   const [timerGoal, setTimerGoal] = useState<number>();
   const timers = useContext(TimerContext);
 
   const onSave = () => {
     timers.add({ name, eventId, logId, durationId, finishAtInGameTime: timerGoal || undefined });
+    onHide();
+  };
+
+  const onHide = () => {
     setName('');
+    setTimerGoal(undefined);
+    _onHide();
   };
 
   return (
     <Modal show={show} title="Vista nýjan teljara" onHide={onHide} primaryBtnText="Vista" onPrimaryBtnClick={onSave}>
       <FormGroup label="Heiti teljara" value={name} onChange={setName} autoFocus></FormGroup>
-      <FormGroup type="number" label="Telja upp í ..." value={timerGoal} onChange={setTimerGoal}></FormGroup>
+      <Form.Label>Telja upp í ...</Form.Label>
+      <InputDuration value={timerGoal} onChange={setTimerGoal}></InputDuration>
     </Modal>
   )
 }
