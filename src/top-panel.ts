9301b5bf3699411ab880ae27797484ba387324a3
Þorvarður Bergmann Kjartansson <thoddi@outlook.com>
2024-04-09 13:10:27 +0000

    Útfært import/export

--- src/top-panel.ts
+++ src/top-panel.ts
@@ -1,3 +1,4 @@
+import { SlDialog, SlTextarea } from '@shoelace-style/shoelace';
 import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
@@ -10,40 +11,68 @@ export class TopPanel extends LitElement {
       justify-content: flex-end;
       border-bottom: 1px solid lightgray;
     }
+    #import-text {
+      margin-bottom: 0.5rem
+    }
+    .settings-dialog {
+      --height: 22rem;
+    }
   `;
 
   render () {
     return html`
       <div class="top-panel">
-        <sl-button variant="default" size="small" circle @click=${this.onClick}>
+        <sl-button variant="default" size="small" circle @click=${this.openDialog}>
           <sl-icon name="gear" label="Settings"></sl-icon>
         </sl-button>
       </div>
-      <sl-dialog label="Dialog" class="settings-dialog">
+      <sl-dialog label="Stillingar" class="settings-dialog">
         <sl-tab-group>
           <sl-tab slot="nav" panel="import">Import</sl-tab>
           <sl-tab slot="nav" panel="export">Export</sl-tab>
 
           <sl-tab-panel name="import">
-            <sl-textarea class="import-textarea"></sl-textarea>
-            <sl-button variant="success" @click=${this.onClick}>
+            <sl-textarea id="import-text"></sl-textarea>
+            <sl-button variant="success" class="mt-1" @click=${this.importData}>
               Vista
             </sl-button>
           </sl-tab-panel>
-          <sl-tab-panel name="export">
-            <sl-textarea class="import-textarea"></sl-textarea>
+          <sl-tab-panel name="export" >
+            <sl-copy-button value="Copied from a custom button" from="export-text.value">
+              <sl-icon slot="copy-icon" name="clipboard"></sl-icon>
+              <sl-icon slot="success-icon" name="clipboard-check"></sl-icon>
+              <sl-icon slot="error-icon" name="clipboard-x"></sl-icon>
+            </sl-copy-button>
+            <sl-textarea id="export-text" disabled>
+            </sl-textarea>
           </sl-tab-panel>
         </sl-tab-group>
       </sl-dialog>
     `;
   }
 
-  onClick() {
-    this.renderRoot?.querySelector('.settings-dialog')?.show();
+  openDialog() {
+    this.renderRoot?.querySelector<SlDialog>('.settings-dialog')?.show();
   }
 
-  connectedCallback() {
-    
+  importData() {
+    const jsonData = this.renderRoot?.querySelector<SlTextarea>('#import-text')?.value;
+    localStorage.clear();
+    if (jsonData) {
+      Object.entries(JSON.parse(jsonData)).forEach(([key, value]) => {
+        localStorage.setItem(key, JSON.stringify(value));
+      });
+    }
+    window.location.reload();
+  }
+
+  firstUpdated() {
+    const exportValue = {} as any;
+    Object.keys(localStorage).forEach((key) => exportValue[key] = JSON.parse(localStorage.getItem(key) ?? '') || undefined);
+    const textArea = this.renderRoot?.querySelector<SlTextarea>('#export-text');
+    if (textArea) {
+      textArea.value = JSON.stringify(exportValue, null, 2);
+    }
   }
 }
 
