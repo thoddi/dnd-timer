5f303a03e87766f3fef7af6b6934303252e9c664
Þorvarður Bergmann Kjartansson <thoddi@outlook.com>
2024-04-10 15:40:47 +0000

    Lagað deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'build',    
+    emptyOutDir: true,
+  },
   plugins: [react({ tsDecorators: true })],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
